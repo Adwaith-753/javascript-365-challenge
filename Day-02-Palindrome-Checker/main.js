2fd88e8930694812f2516f252a54185cbc15c134
adwaith <adwaith@gmail.com>
2026-05-17 19:52:09 +0530

    Added Day 02 challenge edit

---
+++ Day-02-Palindrome-Checker/main.js
@@ -0,0 +1,10 @@
+function Checker(){
+    let checker =document.getElementById("value").value;
+    let result =document.getElementById("result");
+
+    if(checker===""){
+        result.innerHTML="No Value";
+        return;
+    }
+    
+}
