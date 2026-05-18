7b85b67dc6b00905d303924f837caaae0a50478f
adwaith <adwaith@gmail.com>
2026-05-18 21:10:20 +0530

    Added Day 02 challenge edit

--- Day-02-Palindrome-Checker/main.js
+++ Day-02-Palindrome-Checker/main.js
@@ -1,10 +1,19 @@
 function Checker(){
     let checker =document.getElementById("value").value;
     let result =document.getElementById("result");
+    let reversed = "";
 
     if(checker===""){
         result.innerHTML="No Value";
         return;
     }
-    
+
+    for(let i=checker.length-1;i>=0;i--){
+        reversed =reversed+checker[i]
+    }
+    if(checker === reversed){
+        result.innerHTML="its a palindrome number"
+    }else{
+        result.innerHTML="Not a palindrome number"
+    }
 }
